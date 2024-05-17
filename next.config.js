b68403844a6fc91564a412dd0c5b83a90a874ee4
KINGDOMNVHAI <30685314+KINGDOMNVHAI@users.noreply.github.com>
2024-05-17 16:42:50 +0700

    Config Locale

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+// const nextConfig = {
+//     i18n: {
+//         locales: ['en', 'es', 'pt'],
+//         defaultLocale: 'en',
+//     },
+// }
 
-module.exports = nextConfig
+const withNextIntl = require('next-intl/plugin')(
+    // This is the default (also the `src` folder is supported out of the box)
+    './i18n.js'
+);
+
+module.exports = withNextIntl
